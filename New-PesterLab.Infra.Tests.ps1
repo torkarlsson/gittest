e422dc75c75c0f19fe3815a4c2ad71f393ae9c39
Tor Karlsson <tor.karlsson@evry.com>
2018-03-26 09:51:28 +0200

    infratest

---
+++ New-PesterLab.Infra.Tests.ps1
@@ -0,0 +1,22 @@
+﻿$resourceGroup = "testdevoppipe"
+
+Describe "Resource Group tests" -tag "AzureInfrastructure" {
+    
+    Context "Resource Groups" {
+        It "Check Main Resource Group $resourceGroup Exists" {
+            Get-AzureRmResourceGroup -Name $resourceGroup -ErrorAction SilentlyContinue | Should Not be $null
+        }
+   
+    }
+}
+
+Describe "Hub Tests" -tag "AzureInfrastructure" {
+    Context "Notification" {
+        $nHub=Get-AzureRmNotificationHub -Name "GEN-UNIQUE" -ResourceGroupName $resourceGroup -ErrorAction SilentlyContinue
+
+        it "Check Hub $nHub Exists" {
+            $nHub | Should Not be $null
+        }
+         
+    }
+}
